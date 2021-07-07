bf70f5abf4f257eb0ca19390a6627d62c725959e
habiyanlazuard <45533483+habiyanlazuard@users.noreply.github.com>
2021-07-07 20:50:24 +0700

    Add files via upload

---
+++ kriptografi/NewClass.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kriptografi;
+
+/**
+ *
+ * @author Acer
+ */
+public class NewClass {
+    public static void main(String[] args) {
+        // TODO code application logic here
+    kripto n = new kripto();
+    n.setVisible(true);
+}
+    
+}
---
+++ kriptografi/UIManager.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kriptografi;
+
+/**
+ *
+ * @author habiy
+ */
+class UIManager {
+
+    static void setLookAndFeel(String comjtattooplafacrylAcrylLookAndFeel) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
